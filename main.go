2d66aadb655156a24f194df55aa013a8ef485684
Mathew Hall <mhall.js@gmail.com>
2018-12-30 03:45:45 +0000

    Loop added for printing deck

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
+	cards := []string{"Ace of Spades", newCard()}
+	cards = append(cards, "Six of Spades")
 
-	fmt.Println(card)
+	fmt.Println(cards)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
