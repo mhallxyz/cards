422653514021a5c16294e1aaf884362ef52090e1
Mathew Hall <mhall.js@gmail.com>
2018-12-31 14:38:12 +0000

    Previous functions removed

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := deck{"Ace of Spades", newCard()}
-	cards = append(cards, "Six of Spades")
-
-	fmt.Println(cards)
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
