a58d08fc44f134db63f2265488c3837b877037d8
Mathew Hall <mhall.js@gmail.com>
2018-12-31 14:00:13 +0000

    Deck contrsuction added

--- deck.go
+++ deck.go
@@ -6,6 +6,21 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	// cards := deck{"Ace of Spades", "Five of Diamonds", "Six of Spades", "Queen of Hearts"}
+	cards := deck{}
+	cardSuits := []string{"Diamonds", "Spades", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for i, suit := range cardSuits {
+		for j, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
